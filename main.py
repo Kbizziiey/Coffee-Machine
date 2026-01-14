78ff9e081ebe83e701d7a786cf46bd9c7474adb6
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2026-01-14 11:33:05 +0200

    the coffee machine is completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from money_machine import MoneyMachine
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
+# menu_item = MenuItem()
 
 money_machine.report()
 coffee_maker.report()
@@ -13,5 +14,20 @@ is_on = True
 
 while is_on:
     options = menu.get_items()
-    coffee = input(f"What will you like {options}: ")
+    choice = input(f"What would you like ({options}): ").lower()
+
+    if choice == "off":
+        is_on = False
+
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
 
